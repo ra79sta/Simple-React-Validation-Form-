65c1d777f1639a24020c4af7c2269644e8fec4ff
ra79sta <staca.r@gmail.com>
2020-11-15 01:10:16 +0100

    formating

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { PropTypes } from "prop-types";
 
 const Message = (props) => {
-    const {message} = props
-    return (
-        <div>
-            <h3 className="text-center message">{message}</h3>
-        </div>
-    )
-}
+  const { message } = props;
+  return (
+    <div>
+      <h3 className="text-center message">{message}</h3>
+    </div>
+  );
+};
 
 export default Message;
 
 Message.propTypes = {
-    message: PropTypes.string,
-  };
+  message: PropTypes.string,
+};
